322ff3fa42467120503f8c7af06a86bf3d7064fc
joel <itssme3000@gmail.com>
2020-12-11 19:21:45 +0100

    small changes in example5

--- example5_comment_downloader/main.py
+++ example5_comment_downloader/main.py
@@ -5,7 +5,6 @@ from pr0gramm import *
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-
 try:
     import getpass
 except Exception as e:
@@ -21,20 +20,25 @@ except Exception as e:
 
 api = Api(username, pw)
 
-all_comment = Comments()
+all_comments = Comments()
 
 logging.info("Started crawling")
 
 # change username accordingly if you want to crawl comments from someone else
 for comments in api.get_user_comments_iterator(username, flag=api.calculate_flag(True)):
-    all_comment.extend(comments)
+    all_comments.extend(comments)
     time.sleep(0.3)
 
-    logging.info(f"crawling already found {len(all_comment)} comments")
+    logging.info(f"crawling -> already found {len(all_comments)} comments")
 
-logging.info(f"Found a total of {len(all_comment)} comments")
+logging.info(f"Found a total of {len(all_comments)} comments")
 
 with open("all_comments.json", "w") as writer:
-    writer.write(str(all_comment))
+    writer.write(str(all_comments))
 
-logging.info("Done crawling, wrote all comments in json format to file 'all_comments.json'")
+with open("all_comments_formatted.txt", "w") as writer:
+    writer.write("\n\n------------\n\n".join([comment["content"] for comment in all_comments]))
+
+logging.info(
+    "Done crawling, wrote all comments in json format to file 'all_comments.json' "
+    "and wrote comments in a readable format in 'all_comments_formatted.txt'")
